69dff6c5e2a1449c8a38cd25329ae41450e8a911
bajosoto <sergio.soto@live.com>
2026-01-07 15:23:56 +0100

    rename repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Hayleeiahs-Guide-to-Crafting/',
+  base: '/Hayleeiahs-Crafting/',
   root: './',
   build: {
     outDir: 'dist'
